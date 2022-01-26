e7e61ea52773d1a31dc9bcd9bfcb2a18d9599d2f
lerimeur <gregoirebrasseurpro@gmail.com>
2022-01-25 23:51:48 -0800

    feat: add a basic version of api rest in Go for protips App

---
+++ backend/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"backend/routes"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+type core struct {
+	port   string
+	router *mux.Router
+}
+
+func (c *core) init() {
+	c.port = "8080"
+	c.router = mux.NewRouter()
+	routes.Init(c.router)
+}
+
+func main() {
+	server := new(core)
+	server.init()
+
+	println("Server runs on http://localhost:" + server.port)
+	log.Fatal(
+		http.ListenAndServe(":"+server.port, handlers.CORS(
+			handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+			handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}),
+			handlers.AllowedOrigins([]string{"*"}))(server.router)))
+}
---
+++ protips/android/app/src/main/kotlin/com/example/protips/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.protips
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
